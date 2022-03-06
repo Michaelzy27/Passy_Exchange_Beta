54ddbcece7d584aff8e333e22464d4e7409c4c0b
Umeh Nnaemeka <umehmichael27@gmail.com>
2022-03-06 22:42:08 +0100

    fix

--- app/src/main/java/com/michael/passy_exchange_beta/Splashscreen.java
+++ app/src/main/java/com/michael/passy_exchange_beta/Splashscreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class Splashscreen extends AppCompatActivity {
@@ -15,6 +16,78 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+
+        Log.e("dgd", "hdhd");
+
+        String boy = "Michael";
+
+
+        byte david = -127;
+
+        short pen = -32767;
+
+        int book = 50000000 + 500000000;
+
+        long plate = 223377777;
+
+        float spoons;
+        double car;
+
+
+
+        int x = 5;
+        int y = 500;
+        int f = y / x;
+
+        int xx = 5 + 7;
+
+        int price = 50;
+        String name = "Benjamin";
+
+        int box = 74;
+
+        double spoon = 50;
+
+        double total = box + spoon;
+
+        boolean twin = true; //chidinma
+        boolean twins = false;
+
+        Log.i("result", String.valueOf(total));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         logo = findViewById(R.id.logo);
         logo.setTranslationY(-1000f);
