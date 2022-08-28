27972bb145c33945166a8240a1e860dfd834dd29
Umeh Nnaemeka <umehmichael27@gmail.com>
2022-08-29 00:46:25 +0100

    yoots

--- app/src/main/java/com/michael/passy_exchange_beta/SellCoinsFragment.java
+++ app/src/main/java/com/michael/passy_exchange_beta/SellCoinsFragment.java
@@ -101,11 +101,19 @@ public class SellCoinsFragment extends Fragment {
 
     private List<SneakModel> list() {
         List<SneakModel> sneakList = new ArrayList<>();
-        sneakList.add(new SneakModel("Sneak peek #1", R.drawable.sneak_peek1));
-        sneakList.add(new SneakModel("Sneak peek #2", R.drawable.sneak_peek2));
-        sneakList.add(new SneakModel("Sneak peek #3", R.drawable.sneak_peek3));
-        sneakList.add(new SneakModel("Sneak peek #4", R.drawable.sneak_peek4));
-        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.sneak_peek5));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots5));
+        sneakList.add(new SneakModel("Sneak peek #2", R.drawable.yoots6));
+        sneakList.add(new SneakModel("Sneak peek #3", R.drawable.yoots7));
+        sneakList.add(new SneakModel("Sneak peek #1", R.drawable.yoots8));
+        sneakList.add(new SneakModel("Sneak peek #4", R.drawable.yoots19));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots10));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots11));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots12));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots13));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots14));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots15));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots16));
+        sneakList.add(new SneakModel("Sneak peek #5", R.drawable.yoots4));
 
 
         return sneakList;
